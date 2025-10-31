29f51d46748d38a6c07b71e92996b6525cb55c29
Steven Doran <78985334+S81D@users.noreply.github.com>
2025-10-31 10:11:50 -0500

    Create display_recent_files.py

---
+++ lib/display_recent_files.py
@@ -0,0 +1,8 @@
+import os
+
+# show the most recent PrintDQ files produced
+
+display_command = 'ls -lhv /pnfs/annie/persistent/processed/PrintDQ_metrics/'
+
+os.system(display_command)
+print('\n')
